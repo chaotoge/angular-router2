37ed73b471c49cb67626f2c78ddff7c61b58e314
chaotoge <1215923664@qq.com>
2017-06-07 16:37:05 +0800

    更改为路由模块

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,16 @@ import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent }          from './app.component';
-import { CrisisListComponent }   from './crisis-list.component';
-import { HeroListComponent }     from './hero-list.component';
-import { PageNotFoundComponent } from './not-found.component';
-const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent },
-  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent,
-    HeroListComponent,
-    CrisisListComponent,
-    PageNotFoundComponent
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
