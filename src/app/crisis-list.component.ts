c442f4adf38b9db193cc39988a14cf58687eb07f
chaotoge <1215923664@qq.com>
2017-06-07 15:56:50 +0800

    ；里程碑1 从路由开始

---
+++ src/app/crisis-list.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by @chaotoge on 2017/6/7.
+ */
+import { Component } from '@angular/core';
+@Component({
+  template: `
+    <h2>CRISIS CENTER</h2>
+    <p>Get your crisis here</p>`
+})
+export class CrisisListComponent { }
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by @chaotoge on 2017/6/7.
+ */
+import { Component } from '@angular/core';
+@Component({
+  template: '<h2>Page not found</h2>'
+})
+export class PageNotFoundComponent {}
