bbdcef0487a9f1683f25c2f5dbb1307d2f2e34f1
chaotoge <1215923664@qq.com>
2017-06-09 10:45:50 +0800

    添加了。反导航参数。返回后知道前面进入的那个英雄详情

--- src/app/heroes/hero-detail.component.ts
+++ src/app/heroes/hero-detail.component.ts
@@ -19,7 +19,11 @@ import 'rxjs/add/operator/switchMap';
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
+       <p>
+         <button (click)="gotoHeroes()">Back</button>
+       </p>
     </div>
+    
   `
 })
 export class HeroDetailComponent {
@@ -34,5 +38,13 @@ export class HeroDetailComponent {
     // (+) converts string 'id' to a number
       .switchMap((params: Params) => this.service.getHero(+params['id']))
       .subscribe((hero: Hero) => this.hero = hero);
+  }
+  gotoHeroes() {
+    //this.router.navigate(['/heroes']);
+    let heroId = this.hero ? this.hero.id : null;
+    // Pass along the hero id if available
+    // so that the HeroList component can select that hero.
+    // Include a junk 'foo' property for fun.
+    this.router.navigate(['/heroes', { id: heroId, foo: 'foo' }]);
   }
 }
--- src/app/heroes/hero-list.component.ts
+++ src/app/heroes/hero-list.component.ts
@@ -1,37 +1,35 @@
 /**
  * Created by @chaotoge on 2017/6/7.
  */
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Hero, HeroService }  from './hero.service';
 
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './hero-list.component.html',
   styleUrls: ['./hero-list.component.css'],
-  providers: [HeroService]
+  //providers: [HeroService]
 })
 export class HeroListComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
+  heroes: Observable<Hero[]>;
+  private selectedId: number;
   constructor(
-    private router: Router,
-    private heroService: HeroService,
-  ) { }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    private service: HeroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.heroes = this.route.params
+      .switchMap((params: Params) => {
+        this.selectedId = +params['id'];
+        return this.service.getHeroes();
+      });
   }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    console.log(hero)
+  isSelected(hero: Hero) { return hero.id === this.selectedId; }
+  onSelect(hero: Hero) {
     this.router.navigate(['/hero', hero.id]);
   }
 }
